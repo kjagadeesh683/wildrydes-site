58013f13e14b46f032d0a437b7af74555e39360c
Jagadeesh Kesarapu <37505639+kjagadeesh683@users.noreply.github.com>
2024-09-04 19:34:47 -0400

    Initial Commit

---
+++ wildrydes-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_3IF8a7ESq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2escjs80bs5f5pknu2s2g8dv52', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://eoqao010o5.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
